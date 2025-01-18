0747511d31584aa48637d8218269dfc2ef353fd9
Vadim <maslovvadim398@gmail.com>
2025-01-18 12:49:53 +0300

    Initial commit

---
+++ app/src/androidTest/java/test/CupcakeScreenNavigationTest.kt
@@ -0,0 +1,36 @@
+package test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.example.cupcake.CupcakeApp
+import com.example.cupcake.CupcakeScreen
+import junit.framework.TestCase.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class CupcakeScreenNavigationTest { // Создаем тестовый класс
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupCupcakeNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current).apply {
+                navigatorProvider.addNavigator(ComposeNavigator())
+            }
+            CupcakeApp(navController = navController)
+        }
+        @Test
+        fun cupcakeNavHost_verifyStartDestination() {
+            assertEquals(CupcakeScreen.Start.name, navController.currentBackStackEntry?.destination?.route)
+        }
+    }
+}
